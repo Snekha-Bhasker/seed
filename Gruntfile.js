41e2e54c55a8805ec235ba82c90b3bdfab1e29ff
Daniel Bressan <dbressan42@gmail.com>
2017-05-04 12:50:03 -0600

    rewind, try just basic protactor through grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,8 +117,17 @@ module.exports = function(grunt) {
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
-    // grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'protractor_coverage:local', 'copy:copyBack']);
-    grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'copy:instrument', 'protractor_coverage:local', 'copy:copyBack', 'makeReport']);
+
+    //same as npm test more or less
+    grunt.registerTask('coverage', ['protractor_coverage:local']);
+    
+    //don't use instrumented code
+    // grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'protractor_coverage:local', 'copy:copyBack']);    
+
+    //without coveralls
+    // grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'copy:instrument', 'protractor_coverage:local', 'copy:copyBack', 'makeReport']);
+
+    //with coveralls
     // grunt.registerTask('coverage', ['clean', 'copy:save', 'instrument', 'copy:instrument', 'protractor_coverage:local', 'copy:copyBack', 'makeReport', 'coveralls']);
 
     grunt.registerTask('test', ['coverage']);
